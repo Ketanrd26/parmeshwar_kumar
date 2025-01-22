3d62052533af5fbfea97912ef5a50058e561d87f
Rishabh Khade <89901588+rishabhkhade@users.noreply.github.com>
2025-01-22 19:27:38 +0530

    ok

--- src/comp/beforefooter/BeforeFooter.js
+++ src/comp/beforefooter/BeforeFooter.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import './BeforeFooter.scss'
-import bf_img from "../../assets/about.jpg"
+import React from "react";
+import "./BeforeFooter.scss";
+import bf_img from "../../assets/about.jpg";
 
 function BeforeFooter() {
   return (
     <>
-      <div class="before-footer bg-img-cover">
-        <div className='bf-left'>
-          <h2>Get in touch!</h2>
-      
-          <input type='text' placeholder='Name' />
-          <input type='text' placeholder='Email' />
-          <input type='text' placeholder='Contact' />
-          <textarea type='text' placeholder='Message' />
-          <button className='btn'>Submit</button>
+      <div class="parent">
+        <div class="cont">
+          <div class="before-footer bg-img-cover">
+            <div className="bf-left">
+              <h2>Get in touch!</h2>
 
-          <div className='bf-backgound-img bg-img-contain'></div>
-        </div>
-        <div className='bf-image'>
-          <img src={bf_img} />
-        </div>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Email" />
+              <input type="text" placeholder="Contact" />
+              <textarea type="text" placeholder="Message" />
+              <button className="btn">Submit</button>
 
+              <div className="bf-backgound-img bg-img-contain"></div>
+            </div>
+            <div className="bf-image">
+              <img src={bf_img} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default BeforeFooter
+export default BeforeFooter;
